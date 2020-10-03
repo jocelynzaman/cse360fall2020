ee39083b837ad8490172f4037a058118b2607e13
Jocelyn Zaman <44142922+jocelynzaman@users.noreply.github.com>
2020-10-03 11:11:35 -0700

    first commit

---
+++ cse360assignment02/AddingMachine.java
@@ -0,0 +1,57 @@
+package cse360assignment02;
+
+/**
+ * A program to do basic addition
+ * 
+ * @author Jocelyn Zaman
+ * @version 2.0
+ * @date 10/2/2020
+ */
+
+public class AddingMachine {
+  private int total;
+  
+  /**
+   * Constructor of class AddingMachine
+   * sets total and mathHistory to its initial values
+   */
+  public AddingMachine () {
+    total = 0;  // not needed - included for clarity
+  }
+  
+  /**
+   * Getter method to get the value of total
+   * @return total
+   */
+  public int getTotal () {
+    return 0;
+  }
+  
+  /**
+   * Method to add value to the total
+   * @param value
+   */
+  public void add (int value) {
+  }
+
+  /**
+   * Method to subtract value from total
+   * @param value
+   */
+  public void subtract (int value) {
+  }
+
+  /**
+   * Method to return the history of transactions
+   * @return mathHistory
+   */
+  public String toString () {
+    return "";
+  }
+
+  /**
+   * Method clears total and history of transactions
+   */
+  public void clear() {
+  }
+}
